c90d03c6e0d830c8e35724bb73e96e71e9722720
Okyaaa <abi.mofra1@gmail.com>
2022-04-13 15:10:49 +0700

    adding component MUI

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,9 +1,19 @@
+/* eslint-disable comma-dangle */
 import React, { useState } from "react";
 import axios from "axios";
 import "./Search.css";
-import SearchForm from "../../components/search-component/SearchForm";
+// import SearchForm from "../../components/search-component/SearchForm";
 import { useSelector, useDispatch } from "react-redux";
 import { saving } from "../../redux/query-actions";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 const Search = () => {
   const currentQuery = useSelector((state) => state.query.value);
@@ -23,8 +33,8 @@ const Search = () => {
           api_key: process.env.REACT_APP_GIPHY_CLIENTID,
           q: `${currentQuery}`,
           limit: 12,
-          offset: 0
-        }
+          offset: 0,
+        },
       })
       .then((response) => {
         const data = response.data.data;
@@ -38,12 +48,65 @@ const Search = () => {
 
   return (
     <div className="Search">
-      <SearchForm
+      {/* <SearchForm
         query={currentQuery}
         onSubmit={onSubmit}
         handleChange={handleChange}
-      />
-      {searchResult.map((item) => {
+      /> */}
+      <form id="search" className="form" name="search" onSubmit={onSubmit}>
+        {/* <input
+          type="text"
+          id="search"
+          name="search"
+          // query={query}
+          value={props.currentQuery}
+          onChange={props.handleChange}
+          required
+        /> */}
+        <div className="searchInput">
+          <TextField
+            id="outlined-name"
+            label="Name"
+            value={currentQuery}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="buttonSearch">
+          <Button variant="contained" type="submit">
+            Search
+          </Button>
+        </div>
+      </form>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell align="right">Rating</TableCell>
+              <TableCell align="right">Type</TableCell>
+              <TableCell align="right">Images</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {searchResult.map((row) => (
+              <TableRow
+                key={row.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.title}
+                </TableCell>
+                <TableCell align="right">{row.rating}</TableCell>
+                <TableCell align="right">{row.type}</TableCell>
+                <TableCell align="right">
+                  <img src={row.images.fixed_height.url} />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      {/* {searchResult.map((item) => {
         return (
           <img
             // currentQuery={currentQuery}
@@ -53,7 +116,7 @@ const Search = () => {
             key={item.id}
           />
         );
-      })}
+      })} */}
     </div>
   );
 };
